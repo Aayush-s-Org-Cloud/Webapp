fb3cda789bfaf4b48ae98814bd0f0273895b3df4
Aayush Patel <patel.aayush2@northeastern.edu>
2024-10-16 18:23:30 -0400

    env

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -21,19 +21,14 @@ sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/m
 sudo systemctl restart mysql.service
 sudo systemctl enable mysql
 
-# Ensure variables are not empty
-if [ -z "$DATA_USER" ] || [ -z "$DATA_HOST" ] || [ -z "$DATA_PASSWORD" ] || [ -z "$DATA_DATABASE" ]; then
-  echo "One or more required variables are empty"
-  exit 1
-fi
-
-# Use bash -c to correctly handle special characters in variables
-sudo bash -c "mysql -e \"CREATE USER IF NOT EXISTS '$DATA_USER'@'$DATA_HOST' IDENTIFIED BY '$DATA_PASSWORD';\""
-sudo bash -c "mysql -e \"ALTER USER '$DATA_USER'@'$DATA_HOST' IDENTIFIED WITH mysql_native_password BY '$DATA_PASSWORD';\""
-sudo bash -c "mysql -e \"GRANT ALL PRIVILEGES ON *.* TO '$DATA_USER'@'$DATA_HOST' WITH GRANT OPTION;\""
+sudo mysql -e "CREATE USER IF NOT EXISTS '$DATA_USER'@'$DATA_HOST' IDENTIFIED BY '$DATA_PASSWORD';"
+ 
+sudo mysql -e "ALTER USER '$DATA_USER'@'$DATA_HOST' IDENTIFIED WITH mysql_native_password BY '$DATA_PASSWORD';"
+ 
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DATA_USER'@'$DATA_HOST' WITH GRANT OPTION;"
 
 # Create the database
-sudo bash -c "mysql -e \"CREATE DATABASE $DATA_DATABASE;\""
+sudo mysql -e "CREATE DATABASE $DATA_DATABASE;"
 
 # Flush privileges
 sudo mysql -e "FLUSH PRIVILEGES;"
