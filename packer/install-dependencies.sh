0d8aee1b52850ab9c00c47529f86f2a6ba134864
Aayush Patel <patel.aayush2@northeastern.edu>
2024-10-22 17:27:45 -0400

    removed local database

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -10,29 +10,8 @@ sudo apt-get install zip unzip -y
 # Install Node.js (latest stable version)
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
- 
-# Install MySQL server
-sudo apt-get install -y mysql-server
 
-# Update MySQL configuration to allow remote connections
-sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-
-# Restart MySQL service to apply changes
-sudo systemctl restart mysql.service
-sudo systemctl enable mysql
-
-sudo mysql -e "CREATE USER IF NOT EXISTS 'aayush'@'localhost' IDENTIFIED BY 'Aayush@456';"
- 
-sudo mysql -e "ALTER USER 'aayush'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Aayush@456';"
- 
-sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'aayush'@'localhost' WITH GRANT OPTION;"
-
-# Create the database
-sudo mysql -e "CREATE DATABASE HealthChecker;"
-
-# Flush privileges
-sudo mysql -e "FLUSH PRIVILEGES;"
 
 # Verify installations
 node -v
-mysql --version
+
