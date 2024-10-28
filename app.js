c3ea743b19d9142387548f6009914380d0025026
Aayush Patel <patel.aayush2@northeastern.edu>
2024-10-28 03:49:07 -0400

    image route

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const sequelize = require('./config/database');
 const healthRoutes = require('./routes/health_checker_routes');
 const { handleNotFound } = require('./controller/healthcontroller');
 const userRoutes = require('./routes/user_routes');  
-
+const imageRoutes = require('./routes/image_routes'); 
 app.use('/', express.json());
 app.use('/',userRoutes); 
 app.use('/', healthRoutes);
-
+app.use('/', imageRoutes);
 app.use((err, req, res, next) => {
   if (err.type === 'entity.parse.failed') {
     return res.status(400).json();
