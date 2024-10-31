fb26d581929bc549307a847f99182f628b9bb3c9
Aayush Patel <patel.aayush2@northeastern.edu>
2024-10-31 05:08:00 -0400

    metrics

--- controller/healthcontroller.js
+++ controller/healthcontroller.js
@@ -1,49 +1,34 @@
 const sequelize = require('../config/database');
-const logger = require('../logger'); // Assuming logger is configured properly in the 'logger.js' file
-
 const healthCheck = async (req, res, next) => {
-  // Set headers for caching policies
+  //  headers 
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
   res.setHeader('X-Content-Type-Options', 'nosniff');
-
   try {
-    // Always check database connection first for any request
+    // always checking database connection first for any request 
     await sequelize.authenticate();
-    logger.info('Database connection successful');
-
-    // No method other than GET is allowed
+    // no other than get method is allowed 
     if (req.method !== 'GET') {
-      logger.info(`Method not allowed: ${req.method}`);
-      return res.status(405).send();
+      return res.status(405).send();  
     }
-
-    // For GET requests checking if any content header is greater than 0 which indicates a payload
+    // For GET requests checking any content header iof it is greater than 0 than its a payload 
     if (req.headers['content-length'] && parseInt(req.headers['content-length']) > 0) {
-      logger.info('Payload on GET request where none is expected');
-      return res.status(400).send();
+      return res.status(400).send();  
     }
-
-    // Ensure no query parameters are allowed
     if (Object.keys(req.query).length !== 0) {
-      logger.info('Query parameters not allowed in health check');
       return res.status(400).send();
-    }
-
-    // If everything is fine, return 200 OK
-    res.status(200).send();
-    logger.info('Health check passed successfully');
+  }
+    //ifeverything is fine, return 200 OK
+    res.status(200).send(); 
 
   } catch (error) {
-    logger.error('Database connection error:', error.message);
-    return res.status(503).send();
+    console.error('Database connection error:', error.message);  
+    return res.status(503).send();  
   }
 };
-
-// Handler for unknown pages
+//unknown pages 
 const handleNotFound = (req, res) => {
-  logger.info(`Request made to unknown route: ${req.originalUrl}`);
   return res.status(404).json();
 };
 
--- services/user_service.js
+++ services/user_service.js
@@ -2,17 +2,12 @@ const bcrypt = require('bcryptjs');
 const crypto = require('crypto'); 
 const sequelize = require('../config/database');   
 const User = require('../models/usermodel')(sequelize);
-const logger = require('../logger'); // Ensure logger is correctly configured and imported
+
 
 const isExistingUser = async (email) => {
     const existingUser = await User.findOne({ 
         where: { email: email } 
     });
-    if (existingUser) {
-        logger.info(`User check - existing: ${email}`);
-    } else {
-        logger.info(`User check - not found: ${email}`);
-    }
     return existingUser !== null;  
 };
 
@@ -22,14 +17,12 @@ const createUser = async (userData) => {
     // if user already exists
     const userExists = await isExistingUser(email);
     if (userExists) {
-        logger.info(`Attempt to create user that already exists: ${email}`);
         throw new Error('User with this email already exists');
     }
 
     // password hashing using bcrypt and salt rounds 
     const hashedPassword = await bcrypt.hash(password, 10);
     const token = crypto.randomBytes(48).toString('hex');  
-
     // new user if user is not existing 
     const newUser = await User.create({
         email,
@@ -38,8 +31,6 @@ const createUser = async (userData) => {
         password: hashedPassword,
         token  
     });
-
-    logger.info(`New user created: ${newUser.id}`);
 
     return {
         id: newUser.id,
